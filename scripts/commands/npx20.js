4556a9c3155bc3e792d069b82ebf483e80e22edf
Fahim Islam <160403765+akibul123@users.noreply.github.com>
2024-08-11 11:04:12 +0600

    Update npx20.js

--- scripts/commands/npx20.js
+++ scripts/commands/npx20.js
@@ -26,7 +26,7 @@ const media = (
 
 	if (body.indexOf("😊")==0 || body.indexOf("☺️")==0) {
 		var msg = {
-				body: "𝐌𝐃 𝐉𝐔𝐁𝐀𝐄𝐃 𝐀𝐇𝐌𝐌𝐄𝐃 𝐉𝐎𝐘",
+				body: "md Fahim islam ",
 				attachment: media
 			}
 			api.sendMessage( msg, threadID, messageID);
