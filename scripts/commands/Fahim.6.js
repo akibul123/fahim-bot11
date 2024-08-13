be35cecb899f6efcebd1380b96d753af9a4d6e1a
Fahim Islam <160403765+akibul123@users.noreply.github.com>
2024-08-13 13:18:19 +0600

    Create Fahim.6.js

---
+++ scripts/commands/Fahim.6.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+module.exports = {
+  config:{
+	name: "Fahim.6",
+        version: "1.0.1",
+        prefix: false,
+	permssion: 0,
+	credits: "nayan", 
+	description: "Fun",
+	category: "no prefix",
+	usages: "😒",
+        cooldowns: 5, 
+},
+
+handleEvent: async function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+  const content = event.body ? event.body : '';
+  const body = content.toLowerCase();
+  const axios = require('axios')
+const media = (
+    await axios.get(
+      'https://i.imgur.com/3sdP9dE.mp4',
+      { responseType: 'stream' }
+    )
+  ).data;
+
+	if (body.indexOf("Call a aso")==0 || body.indexOf("Admin")==0) {
+		var msg = {
+				body: "Md Fahim islam ",
+				attachment: media
+			}
+			api.sendMessage( msg, threadID, messageID);
+    api.setMessageReaction("🤣", event.messageID, (err) => {}, true)
+		}
+	},
+	start: function({ nayan }) {
+  }
+}
