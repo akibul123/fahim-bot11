f644495f6a9cc9a4e2b35ba7c4fc64cd3aa6dccb
Fahim Islam <160403765+akibul123@users.noreply.github.com>
2024-08-22 14:01:36 +0600

    Create Fahim.info.js

---
+++ scripts/commands/Fahim.info.js
@@ -0,0 +1,62 @@
+module.exports.config = {
+    name: "Info",
+    version: "1.0.0",
+    permission: 0,
+    credits: "nayan",
+    prefix: true,
+    description: "",
+    category: "prefix",
+    usages: "",
+    cooldowns: 5,
+    dependencies: 
+	{
+    "request":"",
+    "fs-extra":"",
+    "axios":""
+  }
+};
+module.exports.run = async function({ api,event,args,client,Users,Threads,__GLOBAL,Currencies }) {
+const axios = global.nodemodule["axios"];
+const request = global.nodemodule["request"];
+const fs = global.nodemodule["fs-extra"];
+const time = process.uptime(),
+		hours = Math.floor(time / (60 * 60)),
+		minutes = Math.floor((time % (60 * 60)) / 60),
+		seconds = Math.floor(time % 60);
+const moment = require("moment-timezone");
+var juswa = moment.tz("Asia/Dhaka").format("『D/MM/YYYY』 【hh:mm:ss】");
+  
+var callback = () => api.sendMessage({body:``ADMIN AND BOT INFORMATION 
+________________________________________
+
+❇️BOT NAME : FAHIM BOSS🤖🤖
+
+❇️BOT ADMIN : FAHIM ISLAM 
+
+❇️ADDRESS: Faridpur. Dhaka, Bangladesh 
+
+_____________CONTACT_____________
+
+❇️FACEBOOK ID: https://www.facebook.com/profile.php?id=100046430705172
+
+❇️FACEBOOK PAGE: 👅❌
+
+❇️BOT PREFIX : ${global.config.PREFIX}
+
+❇️BOT OWNER : {Fahim  KING} 
+
+OTHER NFORMATION____________________
+
+TYPE /admin 
+
+➟ UPTIME
+
+TODAY IS TIME : ${juswa} 
+
+BOT IS RUNNING ${hours}:${minutes}:${seconds}.
+
+THANKS FOR USING Fahim Islam  『🤖🖤』`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
+    fs.unlinkSync(__dirname + "/cache/1.png"));  
+      return request(encodeURI(`https://graph.facebook.com/100046430705172/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(
+fs.createWriteStream(__dirname+'/cache/1.png')).on('close',() => callback());
+   };
