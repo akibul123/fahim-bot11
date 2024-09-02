ec8b3d054e16657a3c0bbd8141388fa751b41d44
Fahim Islam <160403765+akibul123@users.noreply.github.com>
2024-09-02 13:25:00 +0600

    Update ilove.js

--- scripts/commands/ilove.js
+++ scripts/commands/ilove.js
@@ -13,7 +13,7 @@ module.exports.config = {
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
-	if (event.body.indexOf("Rakhi")==0 || (event.body.indexOf("rakhi")==0 || (event.body.indexOf("@সোহাগ হোসেন")==0 || (event.body.indexOf("@Akhi Akther")==0)))) {
+	if (event.body.indexOf("Rakhi")==0 || (event.body.indexOf("@ভা্ঁব্ঁ দে্ঁখা্ঁর্ঁ টা্ঁই্ঁম্ঁ না্ঁই্ঁ")==0 || (event.body.indexOf("@সোহাগ হোসেন")==0 || (event.body.indexOf("@Akhi Akther")==0)))) {
 		var msg = {
 				body: " ওরে কেউ মেসন দিবি না ও আমার বস ফাহিম এর বউ😇😻 :))"
     }
