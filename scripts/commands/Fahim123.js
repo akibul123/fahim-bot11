3eb5c3a156dee83993d7d32c9250e5358f813a19
Fahim Islam <160403765+akibul123@users.noreply.github.com>
2024-09-02 08:15:51 +0600

    Update Fahim123.js

--- scripts/commands/Fahim123.js
+++ scripts/commands/Fahim123.js
@@ -24,7 +24,7 @@ const media = (
     )
   ).data;
 
-	if (body.indexOf("Valobasa")==0 || body.indexOf("valo")==0 || body.indexOf("ভালোবাসা")==0 || body.indexOf("মায়া")==0 || body.indexOf("Maya")==0 || body.indexOf("Maya")==0 || body.indexOf("maya")==0 || body.indexOf("maya")==0 || body.indexOf("maya")==0 || body.indexOf("maya")==0) {
+	if (body.indexOf("Valobasa")==0 || body.indexOf("valo")==0 || body.indexOf("ভালোবাসা")==0 || body.indexOf("মায়া")==0 || body.indexOf("Maya")==0 || body.indexOf("Maya")==0 || body.indexOf("maya")==0 || body.indexOf("maya")==0 || body.indexOf("maya")==0 || body.indexOf(",")==0) {
 		var msg = {
 				body: "Md Fahim islam",
 				attachment: media
