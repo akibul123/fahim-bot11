f7ab0e04738919c2fb1f4e2eb0265b2043cf318f
Fahim Islam <160403765+akibul123@users.noreply.github.com>
2024-09-02 10:29:35 +0600

    Update Fahim.info.js

--- scripts/commands/Fahim.info.js
+++ scripts/commands/Fahim.info.js
@@ -26,34 +26,36 @@ const moment = require("moment-timezone");
 var juswa = moment.tz("Asia/Manila").format("『D/MM/YYYY』 【hh:mm:ss】");
 var link = ["https://i.imgur.com/jfVTL4T.jpeg", 
 
-            "https://i.imgur.com/jfVTL4T.jpeg", 
+            "https://i.imgur.com/ymHWOTm.jpeg", 
 
-            "https://i.imgur.com/jfVTL4T.jpeg",
+            "https://i.imgur.com/0Gv2AT2.jpeg",
 
-            "https://i.imgur.com/jfVTL4T.jpeg"];
+            "https://i.imgur.com/THk8hul.jpeg"];
 
-var callback = () => api.sendMessage({body:`•—»✨𝐀𝐝𝐦𝐢𝐧 𝐈𝐧𝐟𝐨𝐫𝐦𝐚𝐭𝐢𝐨𝐧✨🌺
- •┄┅════❁🌺❁════┅┄•
+var callback = () => api.sendMessage({body:`ADMIN AND BOT INFORMATION 
+________________________________________
 
-𝐁𝐨𝐭 𝐍𝐚𝐦𝐞 : 𝐈𝐬𝐥𝐚𝐦𝐢𝐜𝐤 𝐂𝐲𝐛𝐞𝐫 𝐂𝐡𝐚𝐭
+❇️BOT NAME : FAHIM BOSS🤖🤖
 
-𝐁𝐨𝐭 𝐀𝐝𝐦𝐢𝐧 : 𝟕 𝐀d𝐦𝐢𝐧 𝐜𝐨𝐧𝐭𝐫𝐨𝐥 𝐓𝐡𝐢𝐬 𝐑𝐨𝐛𝐨𝐭
+❇️BOT ADMIN : FAHIM ISLAM 
 
-•┄┅══❁CONCATET❁══┅┄• 
+❇️ADDRESS: Faridpur. Dhaka, Bangladesh 
 
-𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐏𝐚𝐠𝐞 : https://www.facebook.com/profile.php?id=100081939442749
+_____________CONTACT_____________
 
-𝐖𝐏  : wa.me/+8801859561262
-𝐖𝐏 : wa.me/+880 1309-991724
-𝐖𝐏 : wa.me/+880 1905-600093
-𝐖𝐏 : wa.me/+880 1771-717162
-𝐖𝐏 : wa.me/+880 1885-115218
-𝐖𝐏 : wa.me/+880 1763-899929
-𝐖𝐏 : wa.me/+880 1814-587247
+❇️FACEBOOK ID: https://www.facebook.com/profile.php?id=100046430705172
 
-•┄┅═══❁🌺❁═══┅┄•\n🌺✨𝐎𝐭𝐡𝐞𝐫𝐬 𝐈𝐧𝐟𝐨𝐫𝐦𝐚𝐭𝐢𝐨𝐧✨🌺\n •┄┅═══❁🌺❁═══┅┄•
+❇️FACEBOOK PAGE: 👅❌
 
-TYPE /help
+❇️BOT PREFIX : ${global.config.PREFIX}
+
+❇️BOT OWNER : {Fahim  KING} 
+
+OTHER NFORMATION____________________
+
+TYPE /admin 
+
+➟ UPTIME
 
 𝐁𝐨𝐭 𝐍𝐚𝐦𝐞 : ${global.config.BOTNAME}
 
