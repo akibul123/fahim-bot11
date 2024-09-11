8a1c1f822f6010241c9e211ccf20be852b3ab23b
Fahim Islam <160403765+akibul123@users.noreply.github.com>
2024-09-11 21:31:33 +0600

    Update 01fahim.js

--- scripts/commands/01fahim.js
+++ scripts/commands/01fahim.js
@@ -19,7 +19,7 @@ handleEvent: async function({ api, event, client, __GLOBAL }) {
   const axios = require('axios')
 const media = (
     await axios.get(
-      'https://i.imgur.com/FRCbJBN.mp4',
+      'https://i.imgur.com/tyBwYzg.mp4',
       { responseType: 'stream' }
     )
   ).data;
