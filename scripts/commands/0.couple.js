d5530eda010627f3f30e6ee9611016b03966a327
Fahim Islam <160403765+akibul123@users.noreply.github.com>
2024-09-18 10:04:37 +0600

    Update 0.couple.js

--- scripts/commands/0.couple.js
+++ scripts/commands/0.couple.js
@@ -1,87 +1,40 @@
-/**
-* @author ProCoderMew
-* @warn Do not edit code or edit credits
-*/
-
 module.exports.config = {
-    name: "i431",
-    version: "2.0.0",
-    hasPermssion: 0,
-    credits: "DinhPhuc",
-    description: "",
-    commandCategory: "Love",
-    usages: "[tag]",
-    cooldowns: 5,
-    dependencies: {
-        "axios": "",
-        "fs-extra": "",
-        "path": "",
-        "jimp": ""
-    }
+    name: "rnamebot",
+    version: "1.0.0",
+    hasPermssion: 2,
+    credits: "Fahim islam",
+    description: "Change the nickname of bot in the entire bot!",
+    commandCategory: "System",
+    usages: "[Nickname needs to be placed]",
+    cooldowns: 2,
 };
 
-module.exports.onLoad = async() => {
-    const { resolve } = global.nodemodule["path"];
-    const { existsSync, mkdirSync } = global.nodemodule["fs-extra"];
-    const { downloadFile } = global.utils;
-    const dirMaterial = __dirname + `/cache/`;
-    const path = resolve(__dirname, 'cache', 'Raja2.png');
-    if (!existsSync(dirMaterial + "")) mkdirSync(dirMaterial, { recursive: true });
-    if (!existsSync(path)) await downloadFile("https://scontent.xx.fbcdn.net/v/t1.15752-9/457052121_2452725664929010_8545876069089647479_n.jpg?stp=dst-jpg_p480x480&_nc_cat=109&ccb=1-7&_nc_sid=9f807c&_nc_eui2=AeFmpHJvZzyudrvH1Om5BVxak_o0ET8RsuqT-jQRPxGy6pd15Ay-d7kVLoVp-DUXYL5x6f4bsGWgwTX72S-ZuXxx&_nc_ohc=GQ96beK0vrsQ7kNvgHOGtPN&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&_nc_gid=A3rySk6anxu3dfmDAwnHWcL&oh=03_Q7cD1QGYvVgax1rRNrstS6NHNkeA6k8GmQqgn3LkDF1Dm_Vo_w&oe=67023F1D", path);
-
-}
-
-async function makeImage({ one, two }) {
-    const fs = global.nodemodule["fs-extra"];
-    const path = global.nodemodule["path"];
-    const axios = global.nodemodule["axios"]; 
-    const jimp = global.nodemodule["jimp"];
-    const __root = path.resolve(__dirname, "cache");
-
-    let hon_img = await jimp.read(__root + "https://i.ibb.co/mhxtgwm/49be174dafdc259030f70b1c57fa1c13.jpg");
-    let pathImg = __root + `/Raja2_${one}_${two}.png`;
-    let avatarOne = __root + `/avt_${one}.png`;
-    let avatarTwo = __root + `/avt_${two}.png`;
-    
-    let getAvatarOne = (await axios.get(`https://graph.facebook.com/${one}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: 'arraybuffer' })).data;
-    fs.writeFileSync(avatarOne, Buffer.from(getAvatarOne, 'utf-8'));
-    
-    let getAvatarTwo = (await axios.get(`https://graph.facebook.com/${two}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: 'arraybuffer' })).data;
-    fs.writeFileSync(avatarTwo, Buffer.from(getAvatarTwo, 'utf-8'));
-    
-    let circleOne = await jimp.read(await circle(avatarOne));
-    let circleTwo = await jimp.read(await circle(avatarTwo));
-    hon_img.resize(719, 405).composite(circleOne.resize(150, 150), 515, 107
- ).composite(circleTwo.resize(150, 150), 54, 105);
-    
-    let raw = await hon_img.getBufferAsync("image/png");
-    
-    fs.writeFileSync(pathImg, raw);
-    fs.unlinkSync(avatarOne);
-    fs.unlinkSync(avatarTwo);
-    
-    return pathImg;
-}
-async function circle(image) {
-    const jimp = require("jimp");
-    image = await jimp.read(image);
-    image.circle();
-    return await image.getBufferAsync("image/png");
-}
-
-module.exports.run = async function ({ event, api, args }) {
-    const fs = global.nodemodule["fs-extra"];
-    const { threadID, messageID, senderID } = event;
-    var mention = Object.keys(event.mentions)[0]
-    let tag = event.mentions[mention].replace("@", "");
-    if (!mention) return api.sendMessage("Please tag 1 person", threadID, messageID);
-    else {
-        var one = senderID, two = mention;
-        return makeImage({ one, two }).then(path => api.sendMessage({ body: "This "  +  tag + ' love you so muchÃ°Å¸â€™â€',
-            mentions: [{
-          tag: tag,
-          id: mention
-        }],
-     attachment: fs.createReadStream(path) }, threadID, () => fs.unlinkSync(path), messageID));
+module.exports.run = async ({ event, api, global, args, Threads, client }) => {
+    const custom = args.join(" "),
+            allThread = await Threads.getAll(["threadID"]),
+            idBot = api.getCurrentUserID();
+    var threadError = [],
+        count = 0;
+    if (custom.length != 0) {
+        for (const idThread of allThread) {
+            api.changeNickname(custom, idThread.threadID, idBot, (err) => (err) ? threadError.push(idThread.threadID) : '');
+            count+=1;
+await new Promise(resolve => setTimeout(resolve, 500));
+        }
+return api.sendMessage(`Has changed the name successfully ${count} group`, event.threadID, () => {
+if (threadError != 0) return api.sendMessage("[!] Cannot change the name at" + threadError.lenght + " Group",event.threadID, event.messageID)
+        }, event.messageID);
     }
-                                        }
+    else {
+for (const idThread of allThread) {
+const threadSetting = client.threadSetting.get(idThread.threadID) || {};
+api.changeNickname(`[ ${(threadSetting.hasOwnProperty("PREFIX")) ? threadSetting.PREFIX : global.config.PREFIX} ] • ${(!global.config.BOTNAME) ? "Made by MrTomXxX" : global.config.BOTNAME}`, 
+idThread.threadID, idBot, (err) => (err) ? threadError.push(idThread.threadID) : '');
+            count+=1;
+await new Promise(resolve => setTimeout(resolve, 500));
+        }
+ return api.sendMessage(`Has changed the name successfully ${count} group`, event.threadID, () => {
+if (threadError != 0) return api.sendMessage("[!] Cannot change the name at " + threadError.length + " Group",event.threadID, event.messageID)
+        }, event.messageID);
+    }
+                }
